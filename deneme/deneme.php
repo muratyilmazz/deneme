1bbe6d1f6588a71cc3e97ffca449b853083ffdc5
muratyilmazz <muratyilmazz_@hotmail.com>
2019-08-26 11:57:21 +0300

    ilk proje

---
+++ deneme/deneme.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Bu site bir git deneme sitesidir.
+        </title></head>
+    <body>
+    <h1>deneme</h1>
+        <?php echo 'Bu bir git projesidirç';?>
+
+        
+    </body>
+</html>
